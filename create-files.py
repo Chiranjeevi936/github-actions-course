51f49ac80f9910465c7a5e8b786acb84bb418df4
Chiranjeevi936 <chiranjeevipattigidi@gmail.com>
2025-12-01 15:01:25 +0530

    add download workflow

--- create-files.py
+++ create-files.py
@@ -1,5 +1,18 @@
-files = ["file1.txt", "file2.json", "file3.yaml", "file4.jpeg"]
+from string import ascii_letters
+import random
+
+def generate_random_string() -> str:
+    n = len(ascii_letters)
+    random.randrange(0,n+1)
+    random_string = ''.join( [ str(ascii_letters)[random.randrange(n)] for _ in range(100) ] )
+    return random_string
+
+files = ["file1.txt", "file2.json", "file3.yaml", "file4.txt", "tfile.txt", "gfile.txt"]
 
 for file in files:
     with open(file, "w") as fp:
-        fp.write(file)
+        fp.write(generate_random_string())
+
+
+
+
---
+++ read_files.py
@@ -0,0 +1,10 @@
+from  pathlib import Path
+
+files_list = sorted(Path('.').glob(pattern =  './file*'))
+
+for  file in files_list:
+    content = file.read_text()
+    content_length = len(content)
+    print(str(file.name).center(100,"-"))
+    print(content)
+    print()
